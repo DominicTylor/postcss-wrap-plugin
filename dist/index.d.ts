3dd388eb5ce731ba4d0aef784087323be6b1b54f
DominicTylor <DominicTylor13@gmail.com>
2020-02-16 21:04:01 +0300

    Add readme and turn on terser

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import PostCSS from 'postcss';
-import { IOptions } from './types';
-declare const _default: PostCSS.Plugin<IOptions>;
-export default _default;
--- dist/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type IWrapSelector = string | string[];
-export declare enum IHandleRootTags {
-    'replace' = "replace",
-    'remove' = "remove"
-}
-export declare type IOptions = {
-    wrapSelector: IWrapSelector;
-    handleRootTags?: IHandleRootTags;
-};
